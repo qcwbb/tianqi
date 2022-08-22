3e36cb19ccc24ee8bb04efff886a33d01305e244
qcwbb <71008089+qcwbb@users.noreply.github.com>
2022-08-22 20:57:00 +0800

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ today = datetime.now()
 start_date = os.environ['START_DATE']
 city = os.environ['CITY']
 birthday = os.environ['BIRTHDAY']
-wind = os.environ['WIND']
+
 app_id = os.environ["APP_ID"]
 app_secret = os.environ["APP_SECRET"]
 
